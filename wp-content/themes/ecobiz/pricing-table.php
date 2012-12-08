133216bfd991482a6e7d820ea664387821cee746
David Song <dreewind361@gmail.com>
2012-12-08 22:48:00 +0800

    Modifiy Pricing-table tempalte and css for this page.

--- wp-content/themes/ecobiz/pricing-table.php
+++ wp-content/themes/ecobiz/pricing-table.php
@@ -29,26 +29,27 @@ Template Name: Pricing Table
         <?php $product_desc = get_option('ecobiz_product_desc');?>
         <?php echo stripslashes($product_desc);?>
         <br />
-        <!-- Portfolio Filter //-->
-    			<ul  id="filter">
-          	<?php  
-          	
-          	$product_button_text = get_option('ecobiz_product_button_text');          	
-            $categories = get_categories('taxonomy=product_category&orderby=ID&title_li=&hide_empty=0');
-            foreach ($categories as $category) { 
-            $termlink = get_term_link($category->slug,$category->taxonomy);
-            ?>
-              <li><a  class="<?php if (get_query_var($category->taxonomy) == $category->slug) echo 'current';?>" href="<?php echo $termlink;?>"><?php echo $category->name;?></a></li>
-              <?php
-            }
-            ?>
-        	</ul>
+
+            <ul class="process">
+                <li>1. 通过沟通，了解客户需求<br/>
+                    <span>通过电话、电邮或在线等方式沟通，了解网站建设的基本需求。</span></li>
+                <li>2. 分析需求，建站方案策划</li>
+                <li>3. 确定合作，签署网站建设合同<br/>
+                    <span>双方就项目内容和具体需求进行商谈，认可后签署建站合同。客户支付建设首款。</span></li>
+                <li>4. 网站设计制作，程序开发<br/>
+                    <span>根据策划，按照时间流程安排，进行设计制作，并进行程序开发。</span></li>
+                <li>5. 网站整体测试<br/>
+                    <span>网站设计制作、程序开发完成，进行测试，检查BUG并及时进行修改。</span></li>
+                <li>6. 培训，上线<br/>
+                    <span>网站设计、制作、修改、程序开发完成后，提交给客户审核，客户确认并支付余款。创源网络做为专业的网站建设公司会对维护人员进行培训后正式上线。</span></li>
+            </ul>
     			<div class="clear"></div>        
           <?php
           	global $post;
           	
           	$product_order = get_option('ecobiz_product_order') ? get_option('ecobiz_product_order') : "date";
             $product_cat = get_option('ecobiz_product_cat');
+
             $currency = get_option('ecobiz_currency');
             $billing_cycle = get_option('ecobiz_billing_cycle');            
             $product_button_text = get_option('ecobiz_product_button_text');
@@ -73,7 +74,7 @@ Template Name: Pricing Table
                 <h4><?php the_title();?></h4>
               </div>
               <div class="price">
-                <span class="currency"><?php echo $currency ? $currency : "&#36;";?></span><span class="price"><?php echo $product_price;?></span><span class="month">/<?php if ($billing_cycle == "monthly") echo 'month'; else echo 'year';?></span>
+                <span class="currency"><?php echo $currency ? $currency : "&#36;";?></span><span class="price"><?php echo $product_price;?></span><span class="month">/<?php if ($billing_cycle == "monthly") echo '月'; else echo '年';?></span>
               </div>
               <div class="clear"></div>
               <div class="description">
